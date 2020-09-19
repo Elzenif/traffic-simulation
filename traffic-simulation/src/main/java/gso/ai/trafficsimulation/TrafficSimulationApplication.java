041f4fa1b3b8adef35968a8ef7e2107cb7e1b767
Elzenif <shigo.germain@gmail.com>
2020-09-19 12:09:52 +0200

    First commit

---
+++ traffic-simulation/src/main/java/gso/ai/trafficsimulation/TrafficSimulationApplication.java
@@ -0,0 +1,13 @@
+package gso.ai.trafficsimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficSimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficSimulationApplication.class, args);
+	}
+
+}
---
+++ traffic-simulation/src/test/java/gso/ai/trafficsimulation/TrafficSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package gso.ai.trafficsimulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficSimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
